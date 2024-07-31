c05b7b0f816bc2d26d24f05969fa2592c8774a01
Ethan Coe-Renner <e.coerenner@gmail.com>
2024-07-31 01:16:40 -0700

    fix a error in zsh antigen setup

--- run_once_after_setup-zsh.sh
+++ run_once_after_setup-zsh.sh
@@ -2,6 +2,7 @@
 
 antigen_file="$HOME"/.config/zsh/antigen.zsh
 if [ ! -f "$antigen_file" ]; then
-	touch "$antigen_file"
+	# TODO: try to simplify this
+	mkdir "$HOME"/.config/zsh
 	curl -L git.io/antigen > "$antigen_file"
 fi
