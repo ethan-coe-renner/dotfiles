12ebddba3d429fe852ee6cfd55d44d2399f9033e
Ethan Coe-Renner <coerenner1@llnl.gov>
2024-07-31 15:32:37 -0700

    install antigen with chezmoiexternal instead of a script

--- run_once_after_setup-zsh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-antigen_file="$HOME"/.config/zsh/antigen.zsh
-if [ ! -f "$antigen_file" ]; then
-	# TODO: try to simplify this
-	mkdir "$HOME"/.config/zsh
-	curl -L git.io/antigen > "$antigen_file"
-fi
