cd2a35b8c6448addaad2dae5a7c6f852304aa403
Ethan Coe-Renner <e.coerenner@gmail.com>
2024-07-31 21:57:00 -0700

    add complete test for zsh config

---
+++ ci/test_zsh.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+# Exit immediately if any command fails
+set -e
+
+# Enable extended debugging to catch warnings and errors
+set -x
+
+# Check configuration
+source ~/.zshrc
+
+# In the future we could add some more tests here
+
+exit 0
