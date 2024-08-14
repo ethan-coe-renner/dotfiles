1de55e66d1c475dda3f0cedec735db1b6a9dc3d4
Ethan Coe-Renner <e.coerenner@gmail.com>
2024-08-13 21:46:18 -0700

    stop kitty path from printing during init

--- .chezmoiscripts/run_once_after_miscellaneous.sh
+++ .chezmoiscripts/run_once_after_miscellaneous.sh
@@ -3,6 +3,6 @@
 # This is used for various miscellaneous configuration
 
 # Set the kitty theme
-if command -v kitten ; then
+if command -v kitten &> /dev/null; then
     kitten themes Gruvbox Dark Hard
 fi
