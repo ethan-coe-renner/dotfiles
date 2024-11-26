510894ce943c3bfeb8c904549e388278fb86052e
Ethan Coe-Renner <coerenner1@llnl.gov>
2024-11-26 11:26:29 -0800

    simplify nu_start script

--- dot_config/nushell/nu_start.sh
+++ dot_config/nushell/nu_start.sh
@@ -2,25 +2,14 @@
 command -v nu &> /dev/null || return
 
 # Don't run zellij when connected via ssh
+# TODO: maybe run tmux on ssh sessions, usually it is installed
 [[ $SESSION_TYPE == remote/ssh ]] && export NO_ZELLIJ=1
 
-# Disable zellij if it isn't installed
-command -v zellij &> /dev/null || export NO_ZELLIJ=1
-
-export ZELLIJ_AUTO_ATTACH="true"
 export ZELLIJ_AUTO_EXIT="true"
 
 # If zellij is not already running and hasn't been disabled, run it
 if [[ -z "$ZELLIJ" && -z "$NO_ZELLIJ" ]]; then
-    if [[ "$ZELLIJ_AUTO_ATTACH" == "true" ]]; then
-        zellij attach -c
-    else
-        zellij
-    fi
-
-    if [[ "$ZELLIJ_AUTO_EXIT" == "true" ]]; then
-        exit
-    fi
+    zellij && [[ "$ZELLIJ_AUTO_EXIT" == "true" ]] && exit
 fi
 
 # Fallback on nushell
