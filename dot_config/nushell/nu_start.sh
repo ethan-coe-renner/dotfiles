0f4163fe3c6eaff970bab45b632696bbd9bcb1c7
Ethan Coe-Renner <coerenner1@llnl.gov>
2024-11-27 18:07:56 -0800

    don't export NO_ZELLIJ variable

--- dot_config/nushell/nu_start.sh
+++ dot_config/nushell/nu_start.sh
@@ -3,14 +3,14 @@ command -v nu &> /dev/null || return
 
 # Don't run zellij when connected via ssh
 # TODO: maybe run tmux on ssh sessions, usually it is installed
-[[ $SESSION_TYPE == remote/ssh ]] && export NO_ZELLIJ=1
+[[ $SESSION_TYPE == remote/ssh ]] && NO_ZELLIJ=1
 
 export ZELLIJ_AUTO_EXIT="true"
 
 # If zellij is not already running and hasn't been disabled, run it
-if [[ -z "$ZELLIJ" && -z "$NO_ZELLIJ" ]]; then
-    zellij && [[ "$ZELLIJ_AUTO_EXIT" == "true" ]] && exit
-fi
+ if [[ -z "$ZELLIJ" && -z "$NO_ZELLIJ" ]]; then
+     zellij && [[ "$ZELLIJ_AUTO_EXIT" == "true" ]] && exit
+ fi
 
-# Fallback on nushell
-nu && exit
+ # Fallback on nushell
+ nu && exit
