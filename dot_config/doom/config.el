52670b8b3ecba3e0e8549782a44ca7f33ddfd031
Ethan Coe-Renner <e.coerenner@gmail.com>
2024-12-07 21:53:47 -0800

    add splash image for doom dashboard

--- dot_config/doom/config.el
+++ dot_config/doom/config.el
@@ -3,12 +3,6 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
-;; (setq user-full-name "John Doe"
-;;       user-mail-address "john@doe.com")
-
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
 ;; - `doom-font' -- the primary font to use
@@ -78,3 +72,6 @@
 ;; TODO: we could try to update the underlying function (doom/open-private-config)
 ;; to use the correct path
 (assoc-delete-all "Open private configuration" +doom-dashboard-menu-sections)
+
+;; Set splash image for dashboard
+(setq fancy-splash-image (concat doom-user-dir "emacs.png"))
