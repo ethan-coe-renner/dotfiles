fd3c73da43b1d22ac2c8ff710fb7d77ef96111bd
Ethan Coe-Renner <e.coerenner@gmail.com>
2024-12-31 13:27:09 -0800

    fish: fix vi cursor shape in zellij (and others)

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -13,8 +13,25 @@ alias ch="chezmoi"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    set -g fish_key_bindings fish_vi_key_bindings
+
     set fish_greeting
+
+    # --- VI Mode ---
+    set -g fish_key_bindings fish_vi_key_bindings
+    # Emulates vim's cursor shape behavior
+    # Set the normal and visual mode cursors to a block
+    set fish_cursor_default block
+    # Set the insert mode cursor to a line
+    set fish_cursor_insert line
+    # Set the replace mode cursors to an underscore
+    set fish_cursor_replace_one underscore
+    set fish_cursor_replace underscore
+    # Set the external cursor to a line. The external cursor appears when a command is started.
+    # The cursor shape takes the value of fish_cursor_default when fish_cursor_external is not specified.
+    set fish_cursor_external line
+    # The following variable can be used to configure cursor shape in
+    # visual mode, but due to fish_cursor_default, is redundant here
+    set fish_cursor_visual block
 
     set -gx EDITOR nvim
     set -Ux ZELLIJ_AUTO_ATTACH true
@@ -28,7 +45,7 @@ if status is-interactive
 
     zoxide init fish | source
     starship init fish | source
-    # TODO: We don't start zellij until I can figure out how to get vi
-    # cursors to work with fish in zellij
-    # eval (zellij setup --generate-auto-start fish | string collect)
+
+    # Start zellij on startup
+    eval (zellij setup --generate-auto-start fish | string collect)
 end
