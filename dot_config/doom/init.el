6beaf8034d161002899be621ed5f61953308215d
Ethan Coe-Renner <e.coerenner@gmail.com>
2025-01-04 15:59:20 -0800

    emacs: remove +peek from lsp
    suggested by doom doctor

--- dot_config/doom/init.el
+++ dot_config/doom/init.el
@@ -96,7 +96,7 @@
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        (lookup +docsets +dictionary)              ; navigate your code and its documentation
-       (lsp +eglot +peek)               ; M-x vscode
+       (lsp +eglot)               ; M-x vscode
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
