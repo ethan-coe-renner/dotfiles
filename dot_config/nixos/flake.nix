858b56269aeb96dbe882f9978326374a28be805e
Ethan Coe-Renner <e.coerenner@gmail.com>
2025-08-31 18:25:12 -0700

    add nixos flake

---
+++ dot_config/nixos/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    # NixOS official package source
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    kmonad = {
+      url = "github:kmonad/kmonad?dir=nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, kmonad, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        kmonad.nixosModules.default
+      ];
+    };
+  };
+}
